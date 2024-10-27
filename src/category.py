0b1322e34cddd2e0cbe1968845fe1acfc4ac4d9b
Ann Lyapunova <lord.letopisniy@yandex.ru>
2024-10-27 14:24:38 +0300

    new files from old project cause pytest didnt work

---
+++ src/category.py
@@ -0,0 +1,43 @@
+from src.product import Product
+
+
+class Category:
+    name: str
+    description: str
+    products: list
+
+    count_category = 0
+    count_products = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+
+        Category.count_category += 1
+        Category.count_products = len(self.__products)
+
+    def __str__(self):
+        total_products = 0
+        for product in self.__products:
+            total_products += product.quantity
+        return f"{self.name}, количество продуктов: {total_products} шт."
+
+    @property
+    def products(self):
+        return self.__products
+
+    @products.setter
+    def products(self, products_new: Product):
+        if isinstance(products_new, Product):
+            self.__products.append(products_new)
+            Category.count_products += 1
+        else:
+            raise TypeError
+
+    @property
+    def products_list(self):
+        products_list = ""
+        for product in self.__products:
+            products_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_list
---
+++ src/smartphone.py
@@ -0,0 +1,33 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if type(other) is Product:
+            return self.__price * self.quantity + other.__price * other.quantity
+        raise TypeError
+
+    @classmethod
+    def new_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
